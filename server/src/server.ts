74679dc7d5925d8797431165a4f0f149d4f565cf
SomeYurnero <uniquebizbox@gmail.com>
2022-08-05 13:22:01 +0300

    small changes

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,16 +8,6 @@ const server = new ApolloServer({
     resolvers
 });
 
-pgClient
-    .query(`DELETE FROM users WHERE id = 3`)
-    .then(res => console.log(res.rows))
-    .catch(e => console.error(e.stack))
-
-pgClient
-    .query(`SELECT * FROM users`)
-    .then(res => console.log(res.rows))
-    .catch(e => console.error(e.stack))
-
 // Connecting to heroku postgresql database
 pgClient.connect(error => {
     if (error) return console.log('Database connection failed');
@@ -28,4 +18,3 @@ pgClient.connect(error => {
 server.listen(process.env.PORT).then(data => {
     console.log(`Server is running at ${data.url}`);
 });
-
